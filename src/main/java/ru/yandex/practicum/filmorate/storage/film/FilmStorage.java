39268d9e19491de8f57a9c0a50e22a95c07e5c0c
a.kalyanov <av.kalyanov@yandex.ru>
2023-11-09 20:20:21 +0300

    Sprint 10

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -10,11 +10,14 @@ public interface FilmStorage {
 
     void deleteFilm(int id);
 
-    void updateFilm (Film film);
+    void updateFilm(Film film);
+
     Collection<Film> getAllFilms();
+
     Film getFilmById(int id);
 
-    Film addLike(int filmId,int userId);
-    Film deleteLike(int filmId,int userId);
+    Film addLike(int filmId, int userId);
+
+    Film deleteLike(int filmId, int userId);
 
 }
