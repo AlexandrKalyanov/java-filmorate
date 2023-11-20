e784863cc149723f0cd615e484fbb0a30a806e48
a.kalyanov <av.kalyanov@yandex.ru>
2023-11-20 18:35:18 +0300

    sprint 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.yandex.practicum.filmorate.exceptions.ObjectNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidateException;
@@ -14,18 +15,21 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
     public Map<String, String> handleGeneral(final RuntimeException e) {
         return Map.of("ERROR", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
     public Map<String, String> handleValidateException(final ValidateException e) {
         return Map.of("ERROR", e.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(ObjectNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
     public Map<String, String> handleObjectNotFoundException(final ObjectNotFoundException e) {
         return Map.of("ERROR", e.getMessage());
     }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -5,15 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
-import org.thymeleaf.exceptions.TemplateInputException;
 import ru.yandex.practicum.filmorate.exceptions.ObjectNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.mapper.UserMapper;
 
 import java.sql.Date;
 import java.util.Collection;
-
-import static java.lang.String.format;
 
 @Repository
 @Slf4j
@@ -23,18 +20,18 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User addUser(User user) {
-        template.update("INSERT INTO USERS (email, login, name, birthday) "
+        template.update("INSERT INTO USERS (EMAIL, LOGIN, NAME, BIRTHDAY) "
                         + "VALUES (?, ?, ?, ?)",
                 user.getEmail(),
                 user.getLogin(),
                 user.getName(),
                 Date.valueOf(user.getBirthday()));
-        return template.queryForObject("SELECT id, email, login, name, birthday FROM users WHERE email='" + user.getEmail() + "'", new UserMapper());
+        return template.queryForObject("SELECT USER_ID, EMAIL, LOGIN, NAME, BIRTHDAY FROM users WHERE email= ?", new UserMapper(), user.getEmail());
     }
 
     @Override
     public int deleteUser(int id) {
-        int row = template.update("DELETE FROM USERS where ID = ? ", id);
+        int row = template.update("DELETE FROM USERS where USER_ID = ? ", id);
         if (row == 0){
             throw  new ObjectNotFoundException("Object not found");
         }
@@ -43,18 +40,18 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User updateUser(User user) {
-        template.update("UPDATE USERS SET EMAIL = ?, LOGIN = ?, NAME = ?, BIRTHDAY = ? WHERE id =? ",
+        template.update("UPDATE USERS SET EMAIL = ?, LOGIN = ?, NAME = ?, BIRTHDAY = ? WHERE USER_ID =? ",
                 user.getEmail(),
                 user.getLogin(),
                 user.getName(),
                 Date.valueOf(user.getBirthday()),
                 user.getId());
-        return template.queryForObject(format("SELECT id, email, login, name, birthday FROM users WHERE ID = %d",user.getId()), new UserMapper());
+        return template.queryForObject("SELECT USER_ID, EMAIL, LOGIN, NAME, BIRTHDAY FROM users WHERE USER_ID = ?", new UserMapper(),user.getId());
     }
 
     @Override
     public User getUserById(int id) {
-        return template.queryForObject("SELECT ID, NAME, LOGIN, BIRTHDAY, EMAIL FROM users WHERE ID = '" + id + "'", new UserMapper());
+        return template.queryForObject("SELECT USER_ID, NAME, LOGIN, BIRTHDAY, EMAIL FROM users WHERE USER_ID = ?", new UserMapper(),id);
     }
 
     @Override
@@ -64,28 +61,8 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public boolean existById(int id) {
-       /* try {
-            User user = template.queryForObject("SELECT * FROM USERS WHERE ID = '"+ id +"'", new UserMapper());
-            if (user == null){
-                return false;
-            }return true;
-        }catch (TemplateInputException e){
-            throw new ObjectNotFoundException("объект не найден");
-        }*/
-
-        SqlRowSet userRows = template.queryForRowSet("SELECT * FROM USERS WHERE ID = ?", id);
-        if(userRows.next()) {
-          /* User user = new User(
-                    userRows.getInt("id"),
-                    userRows.getString("email"),
-                    userRows.getString("login"),
-                    userRows.getString("name"),
-                    userRows.getDate("birthday").toLocalDate());*/
-            return true;
-        } else {
-            return false;
-        }
-
+        SqlRowSet userRows = template.queryForRowSet("SELECT * FROM USERS WHERE USER_ID = ?", id);
+        return userRows.next();
     }
 
     @Override
